479c60f5161a64fc2df36af927ec274aed754496
CodingJen <codingjennifer@gmail.com>
2022-09-19 18:40:53 -0600

    Start styling everything

---
+++ src/scripts/jenniferfix.js
@@ -0,0 +1,9 @@
+let flagButton = document.getElementById("flagbutton");
+
+
+function showFlags() {
+    window.location.href = "/pride/flags.html";
+}
+
+
+flagButton.addEventListener("click", showFlags);
