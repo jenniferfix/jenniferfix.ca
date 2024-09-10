e1c28f6bc73efbd528c3065884502ffb2940cb28
Jennifer Fix <codingjennifer@gmail.com>
2024-09-10 16:32:56 -0600

    Add text typer to hero

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,42 @@
+'use client'
 import React from 'react'
 import Socials from './Socials'
+
+const Typer = ({ text, delay }: { text: string; delay: number }) => {
+  const [count, setCount] = React.useState(0)
+  const [curText, setCurText] = React.useState('')
+  const [flash, setFlash] = React.useState(true)
+
+  React.useEffect(() => {
+    let interval: ReturnType<typeof setInterval>
+    if (count >= text.length)
+      interval = setInterval(() => setFlash((prev) => !prev), 500)
+
+    return () => clearInterval(interval)
+  }, [count, text])
+
+  React.useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout>
+    if (count < text.length) {
+      timeout = setTimeout(() => {
+        setCurText((prevText) => prevText + text[count])
+        setCount((prevCount) => prevCount + 1)
+      }, delay)
+    }
+
+    return () => clearTimeout(timeout)
+  }, [count, text, delay])
+  const outChar = flash ? '_' : '\xA0'
+  return <span>{curText + outChar}</span>
+}
 
 const Homepage = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <main>
-        <div className="text-2xl fira-code-normal">Hello, World</div>
+        <div className="text-2xl fira-code-normal">
+          <Typer text="Hello, World" delay={200} />
+        </div>
         <div className="text-5xl my-2">
           I&#39;m <span className="rainbowtext">Jennifer Fix</span>
         </div>
